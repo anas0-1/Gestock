ea88fdbd7a1f9f4d4aef2660db30007824b40015
anas0-1 <anaszrigui01@gmail.com>
2024-08-08 09:39:37 +0100

    supplier fix

--- app/Policies/SupplierPolicy.php
+++ app/Policies/SupplierPolicy.php
@@ -16,24 +16,24 @@ class SupplierPolicy
    
     public function view(User $user, Supplier $supplier): bool
     {
-        return in_array($user->role, ['admin', 'manager']);
+        return in_array($user->role, ['admin', 'user']);
     }
 
    
     public function create(User $user): bool
     {
-        return in_array($user->role, ['admin', 'manager']);
+        return in_array($user->role, ['admin', 'user']);
     }
 
     
     public function update(User $user, Supplier $supplier): bool
     {
-        return in_array($user->role, ['admin', 'manager']);
+        return in_array($user->role, ['admin', 'user']);
     }
 
    
     public function delete(User $user, Supplier $supplier): bool
     {
-        return in_array($user->role, ['admin', 'manager']);
+        return in_array($user->role, ['admin', 'user']);
     }
 }
